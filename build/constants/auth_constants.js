79dfa631017324e4598797d03e4113a71668c227
June Rhomel <junemandigma@gmail.com>
2026-02-18 03:13:26 +0800

    first commit

--- build/constants/auth_constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SALT_ROUNDS = void 0;
-/** Bcrypt salt rounds for password hashing. */
-exports.SALT_ROUNDS = 10;
